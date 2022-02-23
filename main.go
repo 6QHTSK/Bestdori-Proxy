b14c1e3af9cc5b0c6a4db0b788e39c68aca89710
6QHTSK <psk2019@qq.com>
2022-02-23 11:59:04 +0800

    add support heroku

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/6QHTSK/ayachan-bestdoriAPI/router"
+	"os"
 )
 
 func main() {
 	r := router.InitRouter()
-	err := r.Run("0.0.0.0:21104")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "21104"
+	}
+	err := r.Run("0.0.0.0:" + port)
 	if err != nil {
 		return
 	}
